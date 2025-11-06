cea38e402dc757b4581812dcc43ae6468caa83b2
filipjt88 <filip88ks@gmail.com>
2025-11-06 21:05:20 +0100

    login

---
+++ backend/api/login.php
@@ -0,0 +1,22 @@
+<?php
+require_once "../config/db.php";
+
+$data = json_decode(file_get_contents("php://input"),true);
+
+$username = $data = ['username'] ?? '';
+$password = $data = ['password'] ?? '';
+
+if($username === "admin" $password === "admin123") {
+    echo json_encode([
+        "success" => true,
+        "token" => "fake-jwt-token-123"
+    ]);
+} else {
+    http_response_code(401);
+    echo json_encode([
+        "success" => false,
+        "message" => "Neispravan username ili password"
+    ]);
+}
+
+?>

9c3ad8402c0224ce375549fd0b414afef6d09876
filipjt88 <filip88ks@gmail.com>
2025-11-08 22:49:05 +0100

    add session login start

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,22 +1,31 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
 require_once "../config/db.php";
 
 $data = json_decode(file_get_contents("php://input"),true);
 
-$username = $data = ['username'] ?? '';
-$password = $data = ['password'] ?? '';
+$username = trim($data['username'] ?? '');
+$password = trim($data['password'] ?? '');
 
-if($username === "admin" && $password === "admin123") {
-    echo json_encode([
-        "success" => true,
-        "token" => "fake-jwt-token-123"
-    ]);
+if(empty($username) || empty($password)) {
+    echo json_encode(['success' => false, "message" => "Nedostaju vam podaci!"]);
+    exit;
+}
+
+$stmt = $pdo->prepare("SELECT * FROM admins WHERE username = ?");
+$stmt->execute([$username]);
+$admin = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if($admin && password_verify($password, $admin["password"])) {
+    session_start();
+    $_SESSION["admin"] = $admin["username"];
+    echo json_encode(["success" => true, "message" => "Uspesno ste se logovali!"]);
 } else {
-    http_response_code(401);
-    echo json_encode([
-        "success" => false,
-        "message" => "Neispravan username ili password"
-    ]);
+    echo json_encode(["success" => false, "message" => "Neispravan username ili password!"]);
 }
 
 ?>
